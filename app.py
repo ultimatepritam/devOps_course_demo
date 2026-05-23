c6ffeb12c876f73daa55d5b293555ac0e33d414a
Pritam Mondal <ultimaterdp@gmail.com>
2026-05-23 19:45:44 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route("/hello")
+def hello():
+    return jsonify({"message": "Hello DevOps"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
